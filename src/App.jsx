0085cca02dff8f283c2075ea8af60ac4780cbaf7
adrianorinaldi <adriano-rinaldi@hotmail.com>
2024-04-23 19:53:33 -0300

    teste5

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [dados, setDados] = useState([]);
+
+  const buscarTodasContas = async (event) => {
+    try {
+      const response = await axios.get('https://backendsimples-production.up.railway.app/buscar_todas');
+      setDados(response.data);
+    } catch (error) {
+      console.error('Erro:', error);
+      toast.error("Erro ao tentar buscar todas as contas!");
+    }
+  };
 
   return (
     <>
@@ -18,16 +30,28 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={buscarTodasContas}>
+          buscar contas
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+      <table >
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Descrição</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dados.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.nome}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
